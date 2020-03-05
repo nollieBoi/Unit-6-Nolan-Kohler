491ab7568d87480c58f361c3e1b70f525c46294d
nollieBoi <54951811+nollieBoi@users.noreply.github.com>
2020-03-05 09:41:51 -0800

    run

--- src/ArrayListTester.java
+++ src/ArrayListTester.java
@@ -1,28 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class ArrayListTester {
-    public static void removeZeros(ArrayList<Integer> list) {
-        for (int i = 0; i < list.size(); i++) {
-            list.remove(i);
-            i--;
-        }
-    }
-
-    public int countLetters(ArrayList<String> list){
-        int count = 0;
-        for(int i = 0; i < list.size(); i++){
-            String word = list.get(i);
-            count = count + word.length();
-        }
-        return count;
-    }
-
-    public int findPosition(){
-
-    }
-
-        public static void main(String[] args) {
+public class ArrayListTester{
+    public static void main(String[] args) {
         System.out.println("Test Remove Zeros");
         ArrayList<Integer> nums = new ArrayList<>(Arrays.asList(3,4,0,0,1,4,0,6,0));
         System.out.println("Starting List: " + nums);
@@ -45,7 +25,7 @@ public class ArrayListTester {
         System.out.println();
 
         System.out.println("Test parse into array");
-        System.out.println("Expected: [W, e, s, t, , L, i, n, n] \n Actual: " + parseIntoArrayList("West Linn"));
+        System.out.println("Expected: [W, e, s, t,  , L, i, n, n] \n  Actual: " + parseIntoArrayList("West Linn"));
 
         System.out.println();
 
@@ -70,4 +50,52 @@ public class ArrayListTester {
         System.out.println("Total of Purse = $" + myPurse.getTotal());
 
     }
+
+    public static void removeZeros(ArrayList<Integer> list){
+
+        for(int i = list.size() -1; i > 0 ; i--){
+            if(list.get(i) == 0){
+                list.remove(i);
+            }
+        }
+    }
+
+    public static int countLetters(ArrayList <String> list){
+        int characters = 0;
+        for(int i = 0; i < list.size() ; i++){
+            String word = list.get(i);
+            characters = characters + word.length();
+        }
+        return characters;
+    }
+
+    public static int findPosition(int keyValue, ArrayList <Integer> list){
+        for(int i = 0; i < list.size(); i++){
+            if(list.get(i) == keyValue){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static ArrayList parseIntoArrayList(String input){
+        ArrayList<String> letters = new ArrayList<String>(input.length());
+        for(int i = 0; i < input.length(); i++){
+            letters.add(input.substring(i, i+1));
+        }
+        return letters;
+    }
+
+    public static String findMin(ArrayList<Person> people){
+        int min = people.get(0).getAge();
+        int personMin = 0;
+        for(int i = 0; i < people.size() - 1; i++){
+            if(people.get(i).getAge() < min){
+                min = people.get(i).getAge();
+                personMin = i;
+            }
+        }
+        return people.get(personMin).toString();
+    }
+
 }
--- src/FrequencyChart.java
+++ src/FrequencyChart.java
@@ -1,39 +1,76 @@
-/*
 import java.util.Scanner;
 
 public class FrequencyChart {
 
-    //class stuff
+    private int[] list;
 
-    //instance variables
-
-    private int[] beans;
-    //constructor
-    public FrequencyChart(int[] bean){
-        this.beans = bean;
+    public FrequencyChart(int[] list){
+        this.list = list;
     }
 
-    //method
     public String makeChart(){
+        String result = "";
+
+        String line0 = "";
+        String line1 = "";
+        String line2 = "";
+        String line3 = "";
+        String line4 = "";
+        String line5 = "";
+        String line6 = "";
+        String line7 = "";
+        String line8 = "";
+        String line9 = "";
+
+        for(int i : list){
+            if(i >= 1 && i <= 10)
+                line0 += "*";
+            if(i >= 11 && i <= 20)
+                line1 += "*";
+            if(i >= 21 && i <= 30)
+                line2 += "*";
+            if(i >= 31 && i <= 40)
+                line3 += "*";
+            if(i >= 41 && i <= 50)
+                line4 += "*";
+            if(i >= 51 && i <= 60)
+                line5 += "*";
+            if(i >= 61 && i <= 70)
+                line6 += "*";
+            if(i >= 71 && i <= 80)
+                line7 += "*";
+            if(i >= 81 && i <= 90)
+                line8 += "*";
+            if(i >= 91 && i <= 100)
+                line9 += "*";
+
+
+        }
+        result += "1-10   | " + line0 +"\n";
+        result += "11-20  | " + line1 +"\n";
+        result += "21-30  | " + line2 +"\n";
+        result += "31-40  | " + line3 +"\n";
+        result += "41-50  | " + line4 +"\n";
+        result += "51-60  | " + line5 +"\n";
+        result += "61-70  | " + line6 +"\n";
+        result += "71-80  | " + line7 +"\n";
+        result += "81-90  | " + line8 +"\n";
+        result += "91-100 | " + line9 +"\n";
+
+        return result;
 
     }
 
-    //helper methods
-
-    //end class stuff
-
-    //main method to make the chart
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("How many values in your data set?");
+        System.out.println("Enter number of values in data set");
         int size = scan.nextInt();
 
         int[] data = new int[size];
 
-        System.out.println("Enter data value one at a time, followed by enter:");
+        System.out.println("Enter data value and press ENTER one at a time");
         int val = 0;
-
         for(int i = 0; i < data.length; i++){
             val = scan.nextInt();
             data[i] = val;
@@ -44,4 +81,3 @@ public class FrequencyChart {
         System.out.println(chart.makeChart());
     }
 }
-*/
